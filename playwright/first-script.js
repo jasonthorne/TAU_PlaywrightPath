d60870df9526076bcdff9a57b524296ea9ba51fe
jasonthorne <jasonthorne@live.ie>
2023-01-23 16:16:00 +0000

    first playwright script

---
+++ playwright/first-script.js
@@ -0,0 +1,18 @@
+const {chromium} = require ('playwright');
+
+
+// ++++++ IMPORTANT: playwright APIs are asynchronous, so return promise objects.
+//code therefore has to follow the async/await pattern:
+
+//async arrow function, that calls itself:
+(async()=>{
+    //create and launch a browsr (REMEMER the await - needed as were inside an async function):
+    const browser = await chromium.launch({headless:false, slowMo: 100}); //allow head, and give slowmode of 100ms
+    //create a page to exist within the browser:
+    const page = await browser.newPage();
+    //navigate page to google.com:
+    await page.goto('http://google.com');
+   //finally, close browser:
+   await browser.close();
+})();
+
