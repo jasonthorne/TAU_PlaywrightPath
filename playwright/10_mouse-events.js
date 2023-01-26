b547a2d43dc1204c3407d7381600d303e6ec93e9
jasonthorne <jasonthorne@live.ie>
2023-01-26 12:34:05 +0000

    mouse events

--- playwright/10_mouse-events.js
+++ playwright/10_mouse-events.js
@@ -11,16 +11,27 @@ const {chromium} = require ('playwright');
     //create a page to exist within the browser:
     const page = await browser.newPage();
     //navigate page to site:
-    await page.goto('https://google.com');
+    await page.goto('https://paint.js.org');
 
     //------------------------
 
-
+    //move mouse:
+    await page.mouse.move(200,200); //x & y pixel coords
+    //click on cnvas with mouse:
+    await page.mouse.down();
+    //move mouse 400px on x:
+    await page.mouse.move(400,200);
+    //move y:
+    await page.mouse.move(400,400);
+    //move x:
+    await page.mouse.move(200,400);
+    //move y (now back at start, and square complete):
+    await page.mouse.move(200,200);
+    //release mouse:
+    await page.mouse.up();
 
     //------------------------
 
    //finally, close browser:
    await browser.close();
 })();
-
-
